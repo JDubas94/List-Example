603a6701c481bc36ec69694f2f99851dee9424d3
Julia Dubas <uliadubas94@gmail.com>
2022-08-05 13:55:09 +0300

    Add project files.

---
+++ Example/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> dinosaurs = new List<string>();
+
+            dinosaurs.Add("Pachycephalosaurus");
+            dinosaurs.Add("Parasauralophus");
+            dinosaurs.Add("Mamenchisaurus");
+            dinosaurs.Add("Amargasaurus");
+            dinosaurs.Add("Coelophysis");
+            dinosaurs.Add("Oviraptor");
+
+            Console.WriteLine();
+            foreach (string dinosaur in dinosaurs)
+            {
+                Console.WriteLine(dinosaur);
+            }
+
+            dinosaurs.Reverse();
+
+            Console.WriteLine();
+            foreach (string dinosaur in dinosaurs)
+            {
+                Console.WriteLine(dinosaur);
+            }
+
+            dinosaurs.Reverse(1, 4);
+
+            Console.WriteLine();
+            foreach (string dinosaur in dinosaurs)
+            {
+                Console.WriteLine(dinosaur);
+            }
+            Console.ReadLine();
+        }
+    }
+}
